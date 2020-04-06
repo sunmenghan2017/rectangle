2af2dfcaf79344a981eb6b17c3a4a2f619859a28
sunmenghan2017 <1079352098@qq.com>
2020-04-06 15:25:03 +0800

    v0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,31 @@
+/* global module: true */
 module.exports = function (grunt) {
   grunt.initConfig({
-    htmlhint: {
-      options: {
-        htmlhintrc: '.htmlhintrc'
-      },
-      src: ['*.html']
-    },
-    csslint: {
-      options: {
-        csslintrc: '.csslintrc'     
-      },
-      src: ['*.css']   
-    },
     eslint: {
       options: {
         configFile: '.eslintrc.json'
       },
       target: ['*.js']
     },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: '*.css'
+    },
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'
+      },
+      src: '*.html'
+    },
     mocha: {
       test: {
-        src: ['test/index.html'],               
+        src: ['test/index.html'],
       },
       options: {
         run: true,
-        reporter: 'Dot'
+        reporter: 'Spec'
       }
     }
   });
@@ -34,6 +35,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha');
 
-  grunt.registerTask('lint', ['htmlhint','csslint','eslint']);
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('unitTest', ['mocha']);
 };
