c2c70f292d50819af41223447d322623801f0ccf
sunmenghan2017 <1079352098@qq.com>
2020-04-06 16:58:58 +0800

    v1.0

--- calc.js
+++ calc.js
@@ -1,16 +1,4 @@
-// /* exported Rectangle */
-// exports.Rectangle=function(width, height) {
-//   var w = Number(width);
-//   var h = Number(height);
-  
-//   this.area = function() {
-//     return w * h;
-//   };
-  
-//   this.perimeter = function() {
-//     return 2 * (w + h);
-//   };
-// };
+
 /* exported Rectangle, validate, roundFractional */
 function Rectangle(width, height) {
   var w = Number(width),
