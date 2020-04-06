6e7a36b6620fd40a1bba91b001d2b995561ffc68
sunmenghan2017 <1079352098@qq.com>
2020-04-06 16:09:31 +0800

    v1.1

--- rectangle.js
+++ rectangle.js
@@ -1,45 +1,80 @@
-/* global Rectangle, validate: true */
+function Rectangle(e, t) {
+  var r = Number(e)
+    , n = Number(t);
+  function i(e, t) {
+      return Math.round(e * Math.pow(10, t)) / Math.pow(10, t)
+  }
+  this.area = function() {
+      return i(r * n, 2)
+  }
+  ,
+  this.perimeter = function() {
+      return i(2 * (r + n), 2)
+  }
+}
+function validate(e) {
+  var t = {
+      isOK: !1,
+      reason: ""
+  };
+  return "" === e ? t.reason = "不能为空！" : /^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(e) ? Number(e) < 0 ? t.reason = "必须大于零" : t.isOK = !0 : t.reason = "必须是数值",
+  t
+}
+function isLegalKey(e, t, r) {
+  if (/[abcdf-zABCDF-Z`~!@#$%^&*()\-=_+[\]{}|;:'",<>/?\\]/.test(e))
+      return !1;
+  if ("." === e) {
+      if (0 === r || -1 !== t.indexOf("."))
+          return !1;
+      if (1 === r && "-" === t.substring(0, 1))
+          return !1
+  }
+  if ("e" === e) {
+      if (0 === r || -1 !== t.indexOf("e") || -1 !== t.indexOf("E"))
+          return !1;
+      if (1 === r && "-" === t.substring(0, 1))
+          return !1
+  }
+  if ("E" === e) {
+      if (0 === r || -1 !== t.indexOf("e") || -1 !== t.indexOf("E"))
+          return !1;
+      if (1 === r && "-" === t.substring(0, 1))
+          return !1
+  }
+  return !0
+}
 $(function() {
-  var $width = $('#width'),
-      $height = $('#height'),
-      $btnCal = $('#calculate'),
-      $perimeter = $('#perimeter'),
-      $area = $('#area'),
-      $widthValidate = $('#width-validate'),
-      $heightValidate = $('#height-validate'),
-      isPassValidate = false;
-
-  $width.focusout(function() {
-    var result = validate($width.val());
-    isPassValidate = result.isOK;
-    if(!result.isOK) {
-      $widthValidate.html('宽度' + result.reason);
-      $width.select();
-    } else {
-      $widthValidate.html('');
-    }
-  });
-
-  $height.focusout(function() {
-    var result = validate($height.val());
-    isPassValidate = result.isOK;
-    if(!result.isOK) {
-      $heightValidate.html('高度' + result.reason);
-      $height.select();
-    } else {
-      $heightValidate.html('');
-    }
-  });
-
-  $btnCal.click(function(){
-    if(!isPassValidate) return;
-
-    var w = $width.val(),
-        h = $height.val();
-
-    var r = new Rectangle(w, h);
-
-    $perimeter.val(r.perimeter());
-    $area.val(r.area());
-  });
+  var t = $("#width")
+    , r = $("#height")
+    , e = $("#calculate")
+    , n = $("#perimeter")
+    , i = $("#area")
+    , a = $("#width-validate")
+    , u = $("#height-validate")
+    , s = !1;
+  t.focusout(function() {
+      var e = validate(t.val());
+      s = e.isOK,
+      e.isOK ? a.html("") : (a.html("宽度" + e.reason),
+      t.select())
+  }),
+  t.keypress(function(e) {
+      isLegalKey(e.key, e.target.value, e.target.selectionStart) || e.preventDefault()
+  }),
+  r.focusout(function() {
+      var e = validate(r.val());
+      s = e.isOK,
+      e.isOK ? u.html("") : (u.html("高度" + e.reason),
+      r.select())
+  }),
+  r.keypress(function(e) {
+      isLegalKey(e.key, e.target.value, e.target.selectionStart) || e.preventDefault()
+  }),
+  e.click(function() {
+      if (s) {
+          var e = new Rectangle(t.val(),r.val());
+          n.val(e.perimeter()),
+          i.val(e.area())
+      }
+  })
 });
